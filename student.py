799bebbbc6e10737388f0cb1c56dacf64bda7163
maheshg1505 <112669919+maheshg1505@users.noreply.github.com>
2022-09-16 21:09:51 +0530

    Add files via upload

---
+++ student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import os
+os.getcwd()
+
+
+# In[4]:
+
+
+import pandas as pd
+
+
+# In[9]:
+
+
+pd.read_csv("student.tsv",sep="\t")
+
+
+# In[ ]:
+
+
+
+
